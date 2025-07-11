5d8e0640afbeb6c1067ec6f9265224ddef3d19a1
Utibe-lab <yhutee28@gmail.com>
2025-07-11 11:54:56 +0100

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from .routes.chess_routes import chess_bp
-    app.register_blueprint(chess_bp, url_prefix="/chess")
-
-    return app
--- app/services/openings.py
+++
@@ -1,5 +0,0 @@
-def lookup_opening(fen):
-    # You’ll replace this with a database lookup or PGN parser
-    if "rnbqkbnr" in fen:
-        return "Likely Opening: King's Pawn Game"
-    return "Unknown Opening"
